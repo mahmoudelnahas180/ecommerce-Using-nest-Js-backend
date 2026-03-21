9c528c0d057d0ad97c1bd8b99b06ec79b64ec33e
mahmoudelnahas180 <mahmoudelnahas180@gmail.com>
2026-03-21 13:20:33 +0200

    feat: Add review module with CRUD operations, including controller, service, DTO, and schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { SubCategoryModule } from './modules/sub-category/sub-category.module';
 import { ProductModule } from './modules/product/product.module';
 // import { ProductController } from './modules/product/product.controller';
 import { BrandModule } from './modules/brand/brand.module';
+import { ReviewModule } from './modules/review/review.module';
 // sss
 
 @Module({
@@ -24,6 +25,7 @@ import { BrandModule } from './modules/brand/brand.module';
     SubCategoryModule,
     ProductModule,
     BrandModule,
+    ReviewModule,
   ],
   controllers: [],
 })
